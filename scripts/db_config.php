457bef6792ed35ba79f577b8a68141894bd12bd1
Vijayanand Premnath <vpvijayanand@gmail.com>
2024-10-15 10:15:45 +0530

    Update db_config.php

--- scripts/db_config.php
+++ scripts/db_config.php
@@ -1,10 +1,10 @@
 <?php
 // SQL server connection information
 $sql_details = array(
-  'user' => 'ramesh',
-  'pass' => 'ramesh',
-  'db'   => 'ticketing',
+  'user' => 'ticketadmin',
+  'pass' => 'Ticket@123',
+  'db'   => 'ticket_db',
   'host' => 'localhost',
-  'port' => '3307'
+  'port' => '3306'
   // ,'charset' => 'utf8' // Depending on your PHP and MySQL config, you may need this
 );
