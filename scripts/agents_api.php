7c294aff2dbc4d31784cd48f154427be58c926dd
Vijayanand Premnath <vpvijayanand@gmail.com>
2024-10-15 10:47:16 +0530

    Update agents_api.php

--- scripts/agents_api.php
+++ scripts/agents_api.php
@@ -1,13 +1,13 @@
 <?php
 require'db_conn.php';
 
-$sql = 'select agent_id, agent_name from Agents';
+$sql = 'select id, agent_name from Agents';
 $result = $conn->query($sql); 
 
 $agents = [];
 if ($result->num_rows > 0) { 
     while ($row = $result->fetch_assoc()) {
-        $agents[$row['agent_id']] = $row['agent_name'];
+        $agents[$row['id']] = $row['agent_name'];
     }
 } 
 $conn->close();
